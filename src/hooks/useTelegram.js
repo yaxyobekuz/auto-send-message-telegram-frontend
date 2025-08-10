22734017ae195412383d46d570c7f4e251c17109
yaxyobekuz <pubgn9642@gmail.com>
2025-08-10 23:14:54 +0500

    Add redux provider

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -1,21 +1,21 @@
 import { useEffect } from "react";
 
 export default () => {
-  const tg = window.Telegram.WebApp;
+  const tg = window?.Telegram?.WebApp;
 
-  useEffect(() => tg.ready(), []);
+  useEffect(() => tg?.ready(), []);
 
-  const onClose = () => tg.close();
+  const onClose = () => tg?.close();
 
   const openTelegramLink = (url) => {
-    tg.openTelegramLink(`https://t.me/${url}`);
+    tg?.openTelegramLink(`https://t.me/${url}`);
   };
 
   const setHeaderColor = (color) => {
     tg?.setHeaderColor(color);
   };
 
-  const user = tg.initDataUnsafe?.user;
+  const user = tg?.initDataUnsafe?.user;
 
   return { onClose, user, tg, openTelegramLink, setHeaderColor };
 };
