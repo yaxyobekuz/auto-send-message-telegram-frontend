b9aa80508dba7ca962604fdc213b3fa96c784405
yaxyobekuz <pubgn9642@gmail.com>
2025-08-10 16:15:22 +0500

    Add homepage contents

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,20 @@
-import Lottie from "lottie-react";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
+
+// Lottie
+import Lottie from "lottie-react";
+
+// Hooks
+import useTelegram from "../hooks/useTelegram";
+
+// Components
+import ArrowIcon from "../components/ArrowIcon";
+
+// Icons
+import usersIcon from "../assets/icons/users.svg";
+import messagesIcon from "../assets/icons/messages.svg";
+
+// Animated stickers
 import duckShrugging from "../assets/animated/duck-shrugging.json";
 
 const Home = () => {
@@ -9,7 +24,92 @@ const Home = () => {
 };
 
 const AuthenticatedUser = () => {
-  return <div className="">User logged in content</div>;
+  const { user, setHeaderColor } = useTelegram();
+  const { first_name: firstName, photo_url: photoUrl, username } = user || {};
+
+  useEffect(() => {
+    setHeaderColor("#2563eb");
+  }, []);
+
+  return (
+    <div className="">
+      {/* Top */}
+      <div className="bg-gradient-to-b from-blue-600 to-blue-400 pt-5 pb-10 -mb-5 z-0">
+        <div className="flex items-center justify-between gap-3.5 container">
+          <b className="text-xl font-semibold text-white">
+            Xabarlarni boshqarish.
+          </b>
+
+          <Link to="/messages" className="bg-white px-5 py-1.5 rounded-full">
+            Xabarlar
+          </Link>
+        </div>
+      </div>
+
+      {/* Profile */}
+      <div className="flex items-center gap-5 relative z-10 container bg-gradient-to-b from-white to-[#f2f5fc] pt-4 pb-8 rounded-t-3xl">
+        {/* Photo */}
+        <img
+          width={56}
+          height={56}
+          src={photoUrl}
+          alt={`${firstName} profil rasmi`}
+          className="size-14 bg-gray-200 rounded-full"
+        />
+
+        <div className="space-y-1.5">
+          <h1 className="line-clamp-1 text-xl font-medium text-[#333]">
+            {firstName || "Foydalanuvchi"}
+          </h1>
+
+          <p className="text-blue-500">@{username || "username"}</p>
+        </div>
+      </div>
+
+      {/* Sections link */}
+      <div className="container space-y-5">
+        {/* Users */}
+        <Link
+          to="/users"
+          className="flex items-center gap-5 justify-between bg-white p-5 rounded-xl"
+        >
+          <div className="flex items-center gap-5">
+            <img
+              width={48}
+              height={48}
+              src={usersIcon}
+              className="size-12"
+              alt="Foydalanuvchilar"
+            />
+
+            <span className="text-lg">Foydalanuvchilar</span>
+          </div>
+
+          <ArrowIcon />
+        </Link>
+
+        {/* Messages */}
+        <Link
+          to="/messages"
+          className="flex items-center gap-5 justify-between bg-white p-5 rounded-xl"
+        >
+          <div className="flex items-center gap-5">
+            <img
+              width={48}
+              height={48}
+              alt="Xabar"
+              src={messagesIcon}
+              className="size-12"
+            />
+
+            <span className="text-lg">Xabarlar</span>
+          </div>
+
+          <ArrowIcon />
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 const UnauthenticatedUser = () => {
