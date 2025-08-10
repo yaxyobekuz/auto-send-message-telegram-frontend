44c15d00c8a3d30461735dac1d2fac7c72813e06
yaxyobekuz <pubgn9642@gmail.com>
2025-08-10 23:13:10 +0500

    Add store configuration & store slice

---
+++ src/store/features/storeSlice.js
@@ -0,0 +1,45 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: { isLoading: true, hasError: false, data: null },
+  groups: { isLoading: false, hasError: false, data: null },
+  userGroups: { isLoading: false, hasError: false, data: null },
+};
+
+export const storeSlice = createSlice({
+  initialState,
+  name: "store",
+  reducers: {
+    updateDataFromStore: (state, { payload }) => {
+      const { selector, data } = payload || {};
+      if (!state[selector]) return;
+
+      if (!state[selector].data) state[selector].data = {};
+      state[selector].data = { ...state[selector].data, ...data };
+    },
+
+    updateLoadingFromStore: (state, { payload }) => {
+      const { selector, value } = payload || {};
+      if (!state[selector]) return;
+
+      state[selector].isLoading = value;
+    },
+
+    updateErrorFromStore: (state, { payload }) => {
+      const { selector, value } = payload || {};
+      if (!state[selector]) return;
+
+      state[selector].hasError = value;
+    },
+  },
+});
+
+// Export action creators
+export const {
+  updateDataFromStore,
+  updateErrorFromStore,
+  updateLoadingFromStore,
+} = storeSlice.actions;
+
+// Export reducer as default
+export default storeSlice.reducer;
