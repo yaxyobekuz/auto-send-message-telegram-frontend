eeffe9c095611a694862a40dafc44efbfa93026f
yaxyobekuz <pubgn9642@gmail.com>
2025-08-10 16:13:49 +0500

    Add telegram widget cdn

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ["9b3dc9b28787.ngrok-free.app"],
+  },
 });
